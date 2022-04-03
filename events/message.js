7db2739f0e5a300df40b9a98ef735ea340f7d022
ThronDeveloper <91382967+ThronDeveloper@users.noreply.github.com>
2022-04-03 23:00:19 +0300

    Add files via upload

---
+++ events/message.js
@@ -0,0 +1,42 @@
+let { prefix } = require("../ayarlar.json");
+const db = require("nrc.db");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = async(message) => {
+
+      let client = message.client;
+        if (message.author.bot) return; 
+        if (!message.guild) return message.reply("")
+        
+        if (!message.content.startsWith(prefix)) return; 
+      
+        const args = message.content.slice(prefix.length).trim().split(/ +/g); 
+        const cmd = args.shift().toLowerCase(); 
+      
+        if (cmd.length === 0) return; 
+      
+        var command = client.commands.get(cmd); 
+        if (!command) command = client.commands.get(client.aliases.get(cmd));
+      
+      
+        if (command) 
+        {
+          if(message.guild) {
+            if(!message.guild.me.permissions.has("EMBED_LINKS")) return message.channel.send("")
+            if(db.has(`komutkanal_${message.guild.id}`)){
+              if(message.channel.id !== db.fetch(`komutkanal_${message.guild.id}`)) {
+                const embed = new MessageEmbed()
+                .setTitle("HATA!")
+                .setDescription(`
+            `)
+              }
+            }
+        }
+          try {
+            command.calistir(client, message, args, prefix)
+          } catch (error) {
+            console.log(error)
+          }
+        }  else 
+        return message.reply(``)  
+}
